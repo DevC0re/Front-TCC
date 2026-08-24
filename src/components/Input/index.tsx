6bad6bc03471f6f353aa988ab62199e5a9c7c732
luizmiguel1784 <santosalmeidaluizmiguel28@gmail.com>
2026-08-23 23:25:23 -0300

    feat: add Input component with password visibility toggle and styles

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,47 @@
+import type { InputHTMLAttributes } from "react";
+import { useState, } from "react";
+import {Eye, EyeOff} from "lucide-react";
+
+interface InputProps extends InputHTMLAttributes <HTMLInputElement> {
+
+    variant?:"default" |"password";
+    type?:"email"|"text"|"password"
+} 
+
+
+export default function Input({ type, id , placeholder, variant= "default", ...rest }: InputProps ){
+    
+    const [isPasswordVisible, setIsPasswordVisible] = useState(false)
+    
+    const resolvedType = 
+    variant==="password"
+    ?isPasswordVisible
+    ?"text"
+    :"password"
+    :type;
+
+
+    return(
+        <div className="input-wrapper">
+        <input 
+        type = {resolvedType}
+        id = {id}
+        placeholder = {placeholder}
+        {...rest}
+        /> 
+        
+        {variant === "password" && (
+            <button
+                type="button"
+                className="toggle-password"
+                onClick={()=> setIsPasswordVisible((prev)=>!prev)}
+                aria-label={isPasswordVisible ? "Ocultar senha" : "Mostrar senha"}>
+
+                    {isPasswordVisible ? <EyeOff /> : <Eye/>}
+                </button>
+
+        )}
+        
+        </div>
+    );
+}
