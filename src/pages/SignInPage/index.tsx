4af095cb14133b1cb3371d9e0fa1c0a4d9d2a192
luizmiguel1784 <santosalmeidaluizmiguel28@gmail.com>
2026-08-25 23:18:09 -0300

    feat: update SignInPage layout and styles for improved user experience

--- src/pages/SignInPage/index.tsx
+++ src/pages/SignInPage/index.tsx
@@ -1,19 +1,19 @@
 import { Button } from "../../components/Button";
 import { Input } from "../../components/Input";
 import hospital from "../../assets/Hospital.jpg";
-import s from "./SignInPage.module.scss"
+import s from "./SignInPage.module.scss";
 
 interface RegisterFormData {
   name: string;
   email: string;
   password: string;
-  "confirmPassword": string;
+  confirmPassword: string;
 }
 interface RegisterFormError {
   name?: string;
   email?: string;
   password?: string;
-  "confirmPassword"?: string;
+  confirmPassword?: string;
 }
 
 export function RegisterPage() {
@@ -24,30 +24,39 @@ export function RegisterPage() {
       </div>
 
       <div className={s.rightSection}>
-        <h1>Registro</h1>
-        <form>
-          <label htmlFor="name">Nome</label>
-          <Input type="text" id="name" placeholder="Nome completo" />
-
-          <label htmlFor="email">E-mail</label>
-          <Input type="email" id="email" placeholder="Digite seu e-mail" />
-
-          <label htmlFor="password">Senha</label>
-          <Input
-            id="password"
-            placeholder="********"
-            variant="password"
-          />
-
-          <label htmlFor="confirm-password">Confirmar senha</label>
-          <Input
-            id="confirm-password"
-            placeholder="********"
-            variant="password"
-          />
-
+        <h1 className={s.text}>Registro</h1>
+        <form className={s.form}>
+          <div className={s.field}>
+            <label htmlFor="name">Nome</label>
+            <Input type="text" id="name" placeholder="Nome completo" />
+          </div>
+          <div className={s.field}>
+            <label htmlFor="email">E-mail</label>
+            <Input type="email" id="email" placeholder="Digite seu e-mail" />
+          </div>
+          <div className={s.field}>
+            <label htmlFor="password">Senha</label>
+            <Input id="password" placeholder="********" variant="password" />
+          </div>
+          <div className={s.field}>
+            <label htmlFor="confirm-password">Confirmar senha</label>
+            <Input
+              id="confirm-password"
+              placeholder="********"
+              variant="password"
+            />
+          </div>
           <Button type="submit">Criar nova conta</Button>
         </form>
+
+        <div className={s.termsNotice}>
+          <p>Ao continuar, você concorda com nossos</p>
+          <p className={s.terms}>
+            <a href="#">Termos de Serviço</a>
+            <span> e </span>
+            <a href="#">Política de Privacidade</a>
+          </p>
+        </div>
       </div>
     </div>
   );
