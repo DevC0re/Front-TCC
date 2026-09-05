6cf4c6a2e829a81e6c0f6e24b448c8a3ecb9e3c2
luizmiguel1784 <santosalmeidaluizmiguel28@gmail.com>
2026-09-05 15:43:33 -0300

    feat: Set up routing for login, registration, and homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
+import { Homepage } from "./pages/HomePage"
 import { LoginPage } from "./pages/LoginPage"
 import { RegisterPage } from "./pages/SignInPage"
-
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 function App() {
 
   return (
     <>
-    {/* <RegisterPage/> */}
-    <LoginPage/>
+    <BrowserRouter>
+    <Routes>
+    
+    <Route path="/Signup" element= {<RegisterPage/>}/>
+    
+    <Route path="/" element={<LoginPage/>}/>
 
+    <Route path="/Home" element={<Homepage/>}/>
+    
+    </Routes>
+    </BrowserRouter>
 
     </>
   )
