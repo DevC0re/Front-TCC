57a1624a0e20484078bfd49b8ed86a2fe3923778
Luizmiguel1784 <santosalmeidaluizmiguel28@gmail.com>
2026-09-11 11:02:56 -0300

    refactor: Clean up formatting and spacing in HospitalCard component

--- src/components/HospitalCard/index.tsx
+++ src/components/HospitalCard/index.tsx
@@ -1,10 +1,45 @@
 
 interface Data {
-    image: string ;
-    name : string;
-    endereco:string;
+    image: string;
+    name: string;
+    endereco: string;
     situacao: boolean;
     avaliacao: number;
-    type: "SUS"|"Convênio";
-    favorito:boolean;
+    type: "SUS" | "Convênio";
+    favorito: boolean;
+}
+export function HospitalCard({ image, name, endereco, situacao, avaliacao, type, favorito }: Data) {
+
+    return (
+        <div>
+
+            <img src={image} alt="" />
+
+            <div className="Favorite">
+
+                <h1>{name}</h1>
+
+            </div>
+
+            <div>
+                <h1>{endereco}</h1>
+            </div>
+
+            <div>
+
+                <div>
+                    <p></p>
+                </div>
+
+                <div>
+                    <p></p>
+                </div>
+
+                <div>
+                    <p></p>
+                </div>
+
+            </div>
+        </div>
+    )
 }
