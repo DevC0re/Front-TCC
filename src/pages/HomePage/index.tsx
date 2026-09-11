4ee456c310e5f2b9f126ffd8c4d7231e96936664
luizmiguel1784 <santosalmeidaluizmiguel28@gmail.com>
2026-09-10 23:51:48 -0300

    feat: Add Homepage layout with search-bar

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,9 +1,59 @@
+import { Search } from "lucide-react";
+import { Input } from "../../components/Input";
+import s from "./Home.module.scss";
+
+
 export function Homepage() {
 
-    return(
+    return (
         <>
-        <h1>Home</h1>
+            <div className={s.content}>
+                
+               <aside className={s.asSide}>
+                <img src="" alt="Logo" />
+
+                <div className={s.navLinks}>
+                <img src="" alt="Logo" />
+                <img src="" alt="Logo" />
+                <img src="" alt="Logo" />
+                </div>
+                
+               </aside>
+
+            <main className={s.main}>
+
+                <div className={s.searchBar}>
+                    <Input className={s.searchInput}
+                    type="text"
+                    id="search"
+                    placeholder="Busque Hospitais"
+                    variant="search"   
+                    />
+                </div>
+
+                <div className="bestHospital">
+                    <h1>Bem vindo, nome</h1>
+                    <p>Encontre os melhores serviços da sua região</p>
+
+                    <div className="Carrossel">
+                        <img src="" alt="hospitais" />
+                    </div>
+
+                </div>
+
+                <div className="nearHospital">
+                    <h1>Mais proximos á você </h1>
+                    <p>Serviços de saúde pertinho de você</p>
+
+                    <div className="Carrossel">
+                        <img src="" alt="Hospitais proximos" />
+                    </div>
+                </div>
+
+            
+            </main>
+ </div>
         </>
     )
-    
+
 }
