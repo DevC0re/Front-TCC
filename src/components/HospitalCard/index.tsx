02a62adc5afcd654a6885535083f843bc760c745
luizmiguel1784 <santosalmeidaluizmiguel28@gmail.com>
2026-09-11 00:25:12 -0300

    feat: Add HospitalCard component

---
+++ src/components/HospitalCard/index.tsx
@@ -0,0 +1,10 @@
+
+interface Data {
+    image: string ;
+    name : string;
+    endereco:string;
+    situacao: boolean;
+    avaliacao: number;
+    type: "SUS"|"Convênio";
+    favorito:boolean;
+}
